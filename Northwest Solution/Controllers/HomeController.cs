4cae8a5784967d8c769118cf1e26f5a8b8a3b370
Andrew Schwartz <andrewwschwartz@gmail.com>
2017-12-05 14:11:04 -0700

    Created Login in controller and view, added 2 images and linked the index to the login view

--- Northwest Solution/Controllers/HomeController.cs
+++ Northwest Solution/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Northwest_Solution.Controllers
 {
@@ -26,5 +27,38 @@ namespace Northwest_Solution.Controllers
 
             return View();
         }
+
+        // GET: Login 
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //Post for the Login Action Method
+        [HttpPost]
+        public ActionResult Login(FormCollection form, bool rememberMe = false)
+        {
+            String user = form["User Name"].ToString();
+            String password = form["Password"].ToString();
+
+            if (string.Equals(user, "client") && (string.Equals(password, "client")))
+            {
+                FormsAuthentication.SetAuthCookie(user, rememberMe);
+
+                return RedirectToAction("Index");
+
+            }
+            else if (string.Equals(user, "management") && (string.Equals(password, "northwest")))
+            {
+                FormsAuthentication.SetAuthCookie(user, rememberMe);
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+
     }
 }
