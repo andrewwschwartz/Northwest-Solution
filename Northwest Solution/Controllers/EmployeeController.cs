02c414762b6f20d8704ec2a0867fc265e6d277a5
Tanner Morse <Tannermorse33@gmail.com>
2017-12-05 21:04:35 -0700

    added view to employee, create view, thankyou view which return them to orders

--- Northwest Solution/Controllers/EmployeeController.cs
+++ Northwest Solution/Controllers/EmployeeController.cs
@@ -42,6 +42,20 @@ namespace Northwest_Solution.Controllers
 
         public ActionResult CreateWO()
         {
+            List<SelectListItem> tests = new List<SelectListItem>();
+            tests.Add(new SelectListItem { Text = "Biochemical Pharmacology (BP)", Value = "0" });
+            tests.Add(new SelectListItem { Text = "DiscoveryScreen® (DS)", Value = "1" });
+            tests.Add(new SelectListItem { Text = "ImmunoScreen® (IS)", Value = "2" });
+            tests.Add(new SelectListItem { Text = "ProfilingScreen® (PF)", Value = "3" });
+            tests.Add(new SelectListItem { Text = "DiscoveryScreen® (DS)", Value = "4" });
+            tests.Add(new SelectListItem { Text = "ImmunoScreen® (IS)", Value = "5" });
+            ViewBag.tests = tests;
+            ViewBag.AddTests = false;
+            return View();
+        }
+
+        public ActionResult thankYOU()
+        {
             return View();
         }
 
