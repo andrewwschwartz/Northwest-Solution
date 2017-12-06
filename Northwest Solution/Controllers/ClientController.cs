945a355587dc62249d31a23f997133b292d53e31
Tanner Morse <Tannermorse33@gmail.com>
2017-12-05 18:38:50 -0700

    added orders, history and associated views

--- Northwest Solution/Controllers/ClientController.cs
+++ Northwest Solution/Controllers/ClientController.cs
@@ -6,8 +6,10 @@ using System.Web.Mvc;
 
 namespace Northwest_Solution.Controllers
 {
+    
     public class ClientController : Controller
     {
+        
         // GET: Client
         public ActionResult Index()
         {
@@ -19,6 +21,41 @@ namespace Northwest_Solution.Controllers
 
             return View();
         }
+
+        public ActionResult MyOrders(int? i)
+        {
+            
+            if (i == 1)
+            {
+                ViewBag.WasApproved = true;
+            }
+
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Approval()
+        {
+
+            return View();
+        }
+
+        public ActionResult History()
+        {
+
+            return View();
+        }
+
+        public ActionResult HistoryDetails()
+        {
+
+            return View();
+        }
+
+
 
     }
 }
